3c4e44603d8bbcbc57c7b754e0742a920c6f1b44
SimplyComplexity <45217631+SimplyComplexity@users.noreply.github.com>
2021-12-02 00:18:11 +0800

    test

---
+++ app/src/main/java/com/learntodroid/piechartandroid/MainActivity.java
@@ -0,0 +1,140 @@
+package com.learntodroid.piechartandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.widget.AdapterView;
+import android.view.View;
+
+
+import java.util.ArrayList;
+
+
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private PieChart pieChart;
+    private Spinner spinner;
+    private Object AdapterView;
+
+
+    String selectedItem;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        pieChart = findViewById(R.id.activity_main_piechart);
+
+        setupPieChart();
+        loadPieChartData();
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Chapter, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+        TextView tv1 = (TextView)findViewById(R.id.poop);
+        tv1.setText(selectedItem);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+        selectedItem = (String) spinner.getItemAtPosition(pos);
+
+
+    }
+
+    @Override
+    public void onNothingSelected(android.widget.AdapterView<?> adapterView) {
+        selectedItem = (String) spinner.getItemAtPosition(1);
+    }
+
+
+    private void setupPieChart() {
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setCenterText("Results");
+        pieChart.setCenterTextSize(24);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+    }
+
+
+    private void loadPieChartData() {
+
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+
+        entries.add(new PieEntry(0.3f, "Pass"));
+        entries.add(new PieEntry(0.4f, "Fail"));
+        entries.add(new PieEntry(0.3f, "Not done"));
+
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color : ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color);
+        }
+
+        for (int color : ColorTemplate.VORDIPLOM_COLORS) {
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Chapters");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
+
+        pieChart.animateY(1400, Easing.EaseInOutQuad);
+    }
+
+
+}
+
+
+
---
+++ app/src/main/java/com/learntodroid/piechartandroid/SpinnerActivity.java
@@ -0,0 +1,22 @@
+/*package com.learntodroid.piechartandroid;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+
+public class SpinnerActivity extends MainActivity implements AdapterView.OnItemSelectedListener {
+
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        String selectedItem = (String) parent.getItemAtPosition(pos);
+    }
+
+
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
+
+*/
